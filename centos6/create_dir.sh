c52942d228faa29bdf7c3dc48c3a3169ea83a85e
liuxingxing <liuxing007xing@163.com>
2018-03-09 19:52:07 +0800

    nginx自启动

--- centos6/create_dir.sh
+++ centos6/create_dir.sh
@@ -8,7 +8,7 @@ ln -s /data/wwwroot /www/
 mkdir -p /data/wwwroot/{web,log,git}
 mkdir -p /data/wwwroot/log/{web,other}
 mkdir /data/conf
-mkdir /data/conf/{sites-available,shell}	
+mkdir /data/conf/{sites-available,shell}
 mkdir /backup
 ln -s /backup /data/
 
