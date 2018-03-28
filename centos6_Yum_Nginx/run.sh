d56b267deb45d4c4584122ee2a9d2b19f3eff19f
liuxingxing <liuxing007xing@163.com>
2018-03-28 17:55:14 +0800

    修改tag名字

--- centos6_Yum_Nginx/run.sh
+++ centos6_Yum_Nginx/run.sh
@@ -5,7 +5,7 @@ mkdir -p /data/docker/nginx_conf
 cp nginx.conf nginx_main.conf /data/docker/nginx_conf/
 
 #构建镜像
-docker build -t  funet8/centos_Yum_Nginx:v1 .
+docker build -t  funet8/centos6YumNginx:v1 .
 
 #启动容器
 docker run -itd --name centos6nginx \
@@ -14,4 +14,4 @@ docker run -itd --name centos6nginx \
 -v /data/docker/nginx_conf/nginx.conf:/etc/nginx/nginx.conf \
 -v /data/docker/nginx_conf/nginx_main.conf:/etc/nginx/conf.d/nginx_main.conf \
 -v /data/wwwroot/log/nginx/:/var/log/nginx/  \
-funet8/centos_Yum_Nginx:v1
+funet8/centos6YumNginx:v1
