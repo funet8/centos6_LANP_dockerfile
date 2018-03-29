9cc409021d278c522abe416a88b471d98ea9928f
liuxingxing <liuxing007xing@163.com>
2018-03-29 18:30:49 +0800

    去掉 dump.rdb

--- centos6_redis/run.sh
+++ centos6_redis/run.sh
@@ -2,16 +2,15 @@
 
 #创建文件夹、复制nginx配置文件
 mkdir -p /data/docker/redis/
-cp 6379.conf dump.rdb /data/docker/redis/
+cp 6379.conf /data/docker/redis/
 
 #构建镜像
 docker build -t  funet8/centos6redis:v1 .
 
 #启动容器
-docker run -itd --name dockerredis \
+docker run -itd --name dockerRedis \
 --restart always \
 -p 63920:6379 \
 -v /data/docker/redis/6379.conf:/etc/redis/6379.conf \
--v /data/docker/redis/dump.rdb:/data/docker/redis/dump.rdb \
 -v /data/wwwroot/:/data/wwwroot/ \
-funet8/centos6redis:v1
+funet8/centos6redis2:v2
