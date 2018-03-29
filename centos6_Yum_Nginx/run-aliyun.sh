cdd5881fae3e4f7fbcea69072bd2230ebccc455e
liuxingxing <liuxing007xing@163.com>
2018-03-29 08:50:22 +0800

    run-aliyun

---
+++ centos6_Yum_Nginx/run-aliyun.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#创建文件夹、复制nginx配置文件
+mkdir -p /data/docker/nginx_conf
+cp nginx.conf nginx_main.conf /data/docker/nginx_conf/
+
+#构建镜像
+#docker build -t  funet8/centos6nginx:v1 .
+
+#启动容器
+docker run -itd --name dockernginx \
+--restart always \
+-p 80:80 -p 443:443 \
+-v /data/docker/nginx_conf/nginx.conf:/etc/nginx/nginx.conf \
+-v /data/docker/nginx_conf/nginx_main.conf:/etc/nginx/conf.d/nginx_main.conf \
+-v /data/wwwroot/log/nginx/:/var/log/nginx/  \
+registry.cn-shenzhen.aliyuncs.com/funet8/centos6.9-nginx:v1
