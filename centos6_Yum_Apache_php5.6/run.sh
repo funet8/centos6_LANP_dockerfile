07bb35f9aa9300760251d582c78d49b0c2bc3b2c
liuxingxing <liuxing007xing@163.com>
2018-03-29 10:59:22 +0800

    修复bug

--- centos6_Yum_Apache_php5.6/run.sh
+++ centos6_Yum_Apache_php5.6/run.sh
@@ -11,4 +11,5 @@ docker run -itd --name centos6_httpd_php56 \
 -v /data/docker/httpd/httpd.conf:/etc/httpd/conf/httpd.conf \
 -v /data/docker/httpd/php.ini:/etc/php.ini  \
 -v /data/docker/httpd/conf.d/:/etc/httpd/conf.d/  \
+-v /data/wwwroot/:/data/wwwroot/ \
 funet8/centos6_httpd_php56:v1
