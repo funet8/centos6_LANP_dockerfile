97c0717d6f0c9153b05dcff07602487991b79a8f
liuxingxing <liuxing007xing@163.com>
2018-05-09 18:04:41 +0800

    更新文件

--- run-aliyun-mysql-apache-nginx.sh
+++ run-aliyun-mysql-apache-nginx.sh
@@ -33,11 +33,11 @@ registry.cn-shenzhen.aliyuncs.com/funet8/centos6.9-mariadb:v1
 ###构建apache-docker
 mkdir -p /data/docker/httpd/conf.d/
 cd /data/docker/httpd/
-wget https://github.com/funet8/centos6_LANP_dockerfile/blob/master/centos6_Yum_Apache_php5.6/httpd.conf
+wget https://raw.githubusercontent.com/funet8/centos6_LANP_dockerfile/master/centos6_Yum_Apache_php5.6/httpd.conf
 wget https://github.com/funet8/centos6_LANP_dockerfile/blob/master/centos6_Yum_Apache_php5.6/php.ini
 
 cd /data/docker/httpd/conf.d/
-wget https://github.com/funet8/centos6_LANP_dockerfile/blob/master/centos6_Yum_Apache_php5.6/apache_main.conf
+wget https://raw.githubusercontent.com/funet8/centos6_LANP_dockerfile/master/centos6_Yum_Apache_php5.6/apache_main.conf
 
 #启动容器 --link链接mysql容器
 docker run -itd --name centos6_httpd_php56 \
@@ -57,7 +57,7 @@ registry.cn-shenzhen.aliyuncs.com/funet8/centos6.9-httpd-php:v5.7
 ###构建nginx-docker
 mkdir -p /data/docker/nginx_conf/conf.d/
 cd /data/docker/nginx_conf/
-wget https://github.com/funet8/centos6_LANP_dockerfile/blob/master/centos6_Yum_Nginx/nginx.conf
+wget https://raw.githubusercontent.com/funet8/centos6_LANP_dockerfile/master/centos6_Yum_Nginx/nginx.conf
 cd /data/docker/nginx_conf/conf.d/
 wget https://github.com/funet8/centos6_LANP_dockerfile/blob/master/centos6_Yum_Nginx/nginx_main.conf
 
