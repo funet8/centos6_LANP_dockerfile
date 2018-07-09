a9ac49bfe00def41a1f89fa98ef44a924d98c4e5
liuxingxing <liuxing007xing@163.com>
2018-07-09 14:05:32 +0800

    可以解析php

--- run-aliyun-mysql-apache-nginx.sh
+++ run-aliyun-mysql-apache-nginx.sh
@@ -59,6 +59,7 @@ wget https://raw.githubusercontent.com/funet8/centos6_LANP_dockerfile/master/cen
 
 cd /data/docker/httpd/conf.d/
 wget https://raw.githubusercontent.com/funet8/centos6_LANP_dockerfile/master/centos6_Yum_Apache_php5.6/apache_main.conf
+wget https://github.com/funet8/centos6_LANP_dockerfile/blob/master/centos6_Yum_Apache_php5.6/php.conf
 
 #启动容器 --link链接mysql容器
 docker run -itd --name centos6_httpd_php56 \
